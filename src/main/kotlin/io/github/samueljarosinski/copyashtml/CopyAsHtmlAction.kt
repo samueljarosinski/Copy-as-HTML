718cd499e1767fdda6a5941966fa952b36f42d73
Samuel Jarosiński <samueljarosinski@gmail.com>
2020-04-12 22:32:39 +0200

    Copy as HTML

--- src/main/kotlin/io/github/samueljarosinski/copyashtml/CopyAsHtmlAction.kt
+++ src/main/kotlin/io/github/samueljarosinski/copyashtml/CopyAsHtmlAction.kt
@@ -1,8 +1,13 @@
 package io.github.samueljarosinski.copyashtml
 
+import com.intellij.codeInsight.editorActions.TextBlockTransferable
+import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.actionSystem.IdeActions
+import com.intellij.openapi.ide.CopyPasteManager
+import com.intellij.util.ui.TextTransferable
 
 class CopyAsHtmlAction : AnAction() {
 
@@ -12,6 +17,19 @@ class CopyAsHtmlAction : AnAction() {
     }
 
     override fun actionPerformed(event: AnActionEvent) {
+        ActionManager.getInstance().getAction(IdeActions.ACTION_EDITOR_COPY).actionPerformed(event)
 
+        val copyPasteManager = CopyPasteManager.getInstance()
+
+        val textBlockTransferable = copyPasteManager.allContents
+            .filterIsInstance<TextBlockTransferable>()
+            .firstOrNull() ?: return
+
+        val dataFlavor = textBlockTransferable.transferDataFlavors
+            .firstOrNull { it.isMimeTypeEqual("text/html") } ?: return
+
+        dataFlavor.getReaderForText(textBlockTransferable).useLines { lines ->
+            copyPasteManager.setContents(TextTransferable(lines.joinToString(separator = "")))
+        }
     }
 }
